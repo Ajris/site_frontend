4345046271418c581298aac5494804111d31cc52
Ajris <hubiflower@gmail.com>
2019-05-16 23:53:41 +0200

    Using babel

--- src/App.test.js
+++ src/App.test.js
@@ -1,14 +1,14 @@
 // import React from 'react';
-import App from './App';
+// import App from './App';
 import Enzyme, {shallow} from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 
 Enzyme.configure({adapter: new Adapter()});
 
-// describe("search component", () => {
-//   test("renders", () => {
-//     expect(2+2).toBe(4);
-//     // const wrapper = shallow(<App />);
-//     // expect(wrapper.exists()).toBe(true);
-//   })
-// });
+describe("search component", () => {
+  test("renders", () => {
+    expect(2+2).toBe(4);
+    // const wrapper = shallow(<App />);
+    // expect(wrapper.exists()).toBe(true);
+  })
+});
