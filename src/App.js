90621ffcc9ef5f5840d927b02c6a07b316b780ce
Marta <martaswierczewska@wp.pl>
2019-05-20 00:29:12 +0200

    layout and footer added

--- src/App.js
+++ src/App.js
@@ -7,6 +7,16 @@ import Projects from './components/projects.component'
 import Experience from './components/experience.component'
 import Technologies from './components/technologies.component'
 
+import linkedin from './assets/linkedin-in-brands.svg'
+import github from './assets/github-brands.svg'
+import youtube from './assets/youtube-brands.svg'
+import twitter from './assets/twitter-square-brands.svg'
+
+import {Layout, Icon} from 'antd';
+const {Header, Content, Footer} = Layout;
+
+
+
 class App extends Component {
     constructor() {
         super();
@@ -34,31 +44,63 @@ class App extends Component {
         if (!isMobile) {
             return (
                 <Router>
-                    <nav className="navbar navbar-expand-sm bg-dark sticky-top">
-                        <div className="navbar-collapse collapse justify-content-between">
-                            <ul className="navbar-nav">
-                                <li className="nav-item">
-                                    <Link className="nav-link text-white" to="/">Home</Link>
-                                </li>
-                            </ul>
-                            <ul className="navbar-nav ml-auto">
-                                <li className="nav-item">
-                                    <Link className="nav-link text-white" to="/projects">Personal Projects</Link>
-                                </li>
-                                <li className="nav-item">
-                                    <Link className="nav-link text-white" to="/experience">Experience</Link>
-                                </li>
-                                <li className="nav-item">
-                                    <Link className="nav-link text-white" to="/technologies">Technologies</Link>
-                                </li>
-                            </ul>
-                        </div>
-                    </nav>
-
-                    <Route exact path={'/'} component={Home}/>
-                    <Route exact path={'/projects'} component={Projects}/>
-                    <Route exact path={'/experience'} component={Experience}/>
-                    <Route exact path={'/technologies'} component={Technologies}/>
+                    <Layout className="layout">
+                        <Header>
+                            <nav className="navbar navbar-expand-sm bg-dark sticky-top">
+                                <div className="navbar-collapse collapse justify-content-between">
+                                    <ul className="navbar-nav">
+                                        <li className="nav-item">
+                                            <Link className="nav-link text-white" to="/">Home</Link>
+                                        </li>
+                                    </ul>
+                                    <ul className="navbar-nav ml-auto">
+                                        <li className="nav-item">
+                                            <Link className="nav-link text-white" to="/projects">Personal
+                                                Projects</Link>
+                                        </li>
+                                        <li className="nav-item">
+                                            <Link className="nav-link text-white" to="/experience">Experience</Link>
+                                        </li>
+                                        <li className="nav-item">
+                                            <Link className="nav-link text-white" to="/technologies">Technologies</Link>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </nav>
+                        </Header>
+                        <Content>
+                            <Route exact path={'/'} component={Home}/>
+                            <Route exact path={'/projects'} component={Projects}/>
+                            <Route exact path={'/experience'} component={Experience}/>
+                            <Route exact path={'/technologies'} component={Technologies}/>
+                        </Content>
+                        <Footer style={{textAlign: 'center'}}>
+                            <nav className="navbar navbar-expand-sm bg-dark">
+                                <ul className="navbar-nav">
+                                    <li className="nav-item">
+                                        <Link to='//linkedin.com'>
+                                            <img src={linkedin} alt="LinkedIn" height={30}/>
+                                        </Link>
+                                    </li>
+                                    <li className="nav-item">
+                                        <Link to='//github.com'>
+                                            <img src={github} alt="Github" height={30} />
+                                        </Link>
+                                    </li>
+                                    <li className="nav-item">
+                                        <Link to='//youtube.com'>
+                                            <img src={youtube} alt="YouTube" height={30}/>
+                                        </Link>
+                                    </li>
+                                    <li className="nav-item">
+                                        <Link to='//twitter.com'>
+                                            <img src={twitter} alt="Twitter" height={30}/>
+                                        </Link>
+                                    </li>
+                                </ul>
+                            </nav>
+                        </Footer>
+                    </Layout>
                 </Router>
             );
         } else {
