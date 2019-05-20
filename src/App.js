33900a345f559500b1c26a15283b8e438d6161e2
Ajris <hubiflower@gmail.com>
2019-05-20 22:06:43 +0200

    moved top bar

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import Projects from './components/Projects'
 import Experience from './components/Experience'
 import Technologies from './components/Technologies'
 import FooterLinks from "./components/FooterLinks";
+import TopBarComputer from "./components/TopBarComputer";
+import TopBarMobile from "./components/TopBarMobile";
 
 const Layout = require('antd').Layout;
 const {Header, Content} = Layout;
@@ -39,29 +41,7 @@ class App extends Component {
             return (
                 <Router>
                     <Layout className="layout">
-                        <Header>
-                            <nav className="navbar navbar-expand-sm bg-dark sticky-top">
-                                <div className="navbar-collapse collapse justify-content-between py-2">
-                                    <ul className="navbar-nav">
-                                        <li className="nav-item ml-3" style={{fontSize: 25}}>
-                                            <Link className="nav-link text-white" to="/">Home</Link>
-                                        </li>
-                                    </ul>
-                                    <ul className="navbar-nav ml-auto mr-2">
-                                        <li className="nav-item">
-                                            <Link className="nav-link text-white" to="/projects">Personal
-                                                Projects</Link>
-                                        </li>
-                                        <li className="nav-item">
-                                            <Link className="nav-link text-white" to="/experience">Experience</Link>
-                                        </li>
-                                        <li className="nav-item">
-                                            <Link className="nav-link text-white" to="/technologies">Technologies</Link>
-                                        </li>
-                                    </ul>
-                                </div>
-                            </nav>
-                        </Header>
+                        <TopBarComputer/>
 
                         <Content>
                             <Route exact path={'/'} component={Home}/>
@@ -78,41 +58,17 @@ class App extends Component {
             return (
                 <Router>
                     <Layout className="layout">
-                        <Header>
-                            <nav className="navbar navbar-dark navbar-expand-sm bg-dark sticky-top">
-                                <Link className="nav-link text-white" to="/">Home</Link>
-                                <button type="button" className="navbar-toggler collapsed" data-toggle="collapse"
-                                        data-target=".toggle-content" aria-controls="navbarNav" aria-expanded="false"
-                                        aria-label="Toggle navigation ">
-                                    <span className="navbar-toggler-icon"/>
-                                </button>
+                        <TopBarMobile/>
 
-                                <div className="navbar-collapse collapse toggle-content">
-                                    <ul className="navbar-nav mx-auto">
-                                        <li className="nav-item active">
-                                            <Link className="nav-link text-white" to="/projects">Personal
-                                                Projects</Link>
-                                        </li>
-                                        <li className="nav-item">
-                                            <Link className="nav-link text-white" to="/experience">Experience</Link>
-                                        </li>
-                                        <li className="nav-item">
-                                            <Link className="nav-link text-white" to="/technologies">Technologies</Link>
-                                        </li>
-                                    </ul>
-                                </div>
-                            </nav>
-                        </Header>
+                        <Content>
+                            <Route exact path={'/'} component={Home}/>
+                            <Route exact path={'/projects'} component={Projects}/>
+                            <Route exact path={'/experience'} component={Experience}/>
+                            <Route exact path={'/technologies'} component={Technologies}/>
+                        </Content>
+
+                        <FooterLinks/>
                     </Layout>
-
-                    <Content>
-                        <Route exact path={'/'} component={Home}/>
-                        <Route exact path={'/projects'} component={Projects}/>
-                        <Route exact path={'/experience'} component={Experience}/>
-                        <Route exact path={'/technologies'} component={Technologies}/>
-                    </Content>
-
-                    <FooterLinks/>
                 </Router>
             )
         }
