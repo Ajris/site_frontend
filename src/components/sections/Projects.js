f670da6fd044ea6967afede74ac87c92e8f75f1c
Ajris <hubiflower@gmail.com>
2019-05-23 02:37:46 +0200

    small change

--- src/components/sections/Projects.js
+++ src/components/sections/Projects.js
@@ -10,16 +10,16 @@ export default class Projects extends React.Component {
                 </div>
                 <div className="card-lines">
                     <div className="card-deck">
-                        <Card title="HOME 1" text="TEXT 1" footer="footer 1"/>
-                        <Card title="HOME 1" text="TEXT 1" footer="footer 1"/>
-                        <Card title="HOME 1" text="TEXT 1" footer="footer 1"/>
-                        <Card title="HOME 1" text="TEXT 1" footer="footer 1"/>
+                        <Card title="Project 1" text="TEXT 1" footer="footer 1"/>
+                        <Card title="Project 1" text="TEXT 1" footer="footer 1"/>
+                        <Card title="Project 1" text="TEXT 1" footer="footer 1"/>
+                        <Card title="Project 1" text="TEXT 1" footer="footer 1"/>
                     </div>
                     <div className="card-deck">
-                        <Card title="HOME 1" text="TEXT 1" footer="footer 1"/>
-                        <Card title="HOME 1" text="TEXT 1" footer="footer 1"/>
-                        <Card title="HOME 1" text="TEXT 1" footer="footer 1"/>
-                        <Card title="HOME 1" text="TEXT 1" footer="footer 1"/>
+                        <Card title="Project 1" text="TEXT 1" footer="footer 1"/>
+                        <Card title="Project 1" text="TEXT 1" footer="footer 1"/>
+                        <Card title="Project 1" text="TEXT 1" footer="footer 1"/>
+                        <Card title="Project 1" text="TEXT 1" footer="footer 1"/>
                     </div>
                 </div>
             </div>
