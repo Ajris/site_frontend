0a33fd7b1358d7cf1a8f34afb578dcd117ea37d3
Ajris <hubiflower@gmail.com>
2019-05-30 02:29:38 +0200

    Added modification for Cards

--- src/components/sections/Card.js
+++ src/components/sections/Card.js
@@ -7,12 +7,23 @@ export default class Card extends React.Component {
             title: props.title,
             text: props.text,
             footer: props.footer
-        }
+        };
+        this.lastTitle = props.title;
+        this.handleMouseHover = this.handleMouseHover.bind(this);
+        this.handleMouseNotHover = this.handleMouseNotHover.bind(this);
+    }
+
+    handleMouseHover() {
+        this.setState({title:'TYTULIK'});
+    }
+
+    handleMouseNotHover(){
+        this.setState({title: this.lastTitle})
     }
 
     render() {
         return (
-            <div className="card text-white bg-dark">
+            <div onMouseEnter={this.handleMouseHover} onMouseLeave={this.handleMouseNotHover} className="card text-white bg-dark">
                 <div className="card-body">
                     <h5 className="card-title">{this.state.title}</h5>
                     <p className="card-text">{this.state.text}</p>
--- src/components/sections/Technologies.js
+++ src/components/sections/Technologies.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Card from "./Card";
+import {Col, Container, Row} from "react-bootstrap";
 
 export default class Technologies extends React.Component {
     render() {
@@ -8,18 +9,17 @@ export default class Technologies extends React.Component {
                 <div className={'mx-4 mt-3'}>
                     <p className={'topic text-center'}>Technologies</p>
                 </div>
-                <div className="card-lines">
-                    <div className="card-deck">
-                        <Card title="HOME 1" text="TEXT 1" footer="footer 1"/>
-                        <Card title="HOME 1" text="TEXT 1" footer="footer 1"/>
-                        <Card title="HOME 1" text="TEXT 1" footer="footer 1"/>
-                    </div>
-                    <div className="card-deck">
-                        <Card title="HOME 1" text="TEXT 1" footer="footer 1"/>
-                        <Card title="HOME 1" text="TEXT 1" footer="footer 1"/>
-                        <Card title="HOME 1" text="TEXT 1" footer="footer 1"/>
-                    </div>
-                </div>
+                <Container>
+                    <Row>
+                        <Col style={{backgroundColor:'RED'}}>1 of 2</Col>
+                        <Col>2 of 2</Col>
+                    </Row>
+                    <Row>
+                        <Col>1 of 3</Col>
+                        <Col>2 of 3</Col>
+                        <Col>3 of 3</Col>
+                    </Row>
+                </Container>;
             </div>
         )
     }
