d20138ea8fa12084de68d5c33ef54fb25c02e7a4
Ajris <hubiflower@gmail.com>
2019-07-17 17:28:00 +0200

    [curr] Fixing snapshots

--- src/components/sections/__tests__/CardTest.js
+++ src/components/sections/__tests__/CardTest.js
@@ -2,9 +2,9 @@ import React from "react";
 import { create } from "react-test-renderer";
 import ItemCard from "../ItemCard";
 
-// describe("ItemCard component", () => {
-//     test("it matches the snapshot", () => {
-//         const component = create(<ItemCard/>);
-//         expect(component.toJSON()).toMatchSnapshot();
-//     });
-// });
+describe("ItemCard component", () => {
+    test("it matches the snapshot", () => {
+        const component = create(<ItemCard/>);
+        expect(component.toJSON()).toMatchSnapshot();
+    });
+});
--- src/components/sections/__tests__/ProjectsTest.js
+++ src/components/sections/__tests__/ProjectsTest.js
@@ -2,9 +2,9 @@ import React from "react";
 import { create } from "react-test-renderer";
 import Projects from "../Projects";
 
-// describe("Projects component", () => {
-//     test("it matches the snapshot", () => {
-//         const component = create(<Projects/>);
-//         expect(component.toJSON()).toMatchSnapshot();
-//     });
-// });
+describe("Projects component", () => {
+    test("it matches the snapshot", () => {
+        const component = create(<Projects/>);
+        expect(component.toJSON()).toMatchSnapshot();
+    });
+});
